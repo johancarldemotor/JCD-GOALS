6125f8c293b661013c0726aea41a00c3c40c2e17
JohanCarlDemotor <hancarldemotor@gmail.com>
2025-08-20 09:30:40 +0800

    second commit

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ const Home = () => {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>
-        J C D G O A L S
+        J C D G O A L
       </Text>
       <Link style={styles.link} href="/goals">
         View Your Goals
