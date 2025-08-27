4b6972479f95cb134f98915123113c69c6084641
JohanCarlDemotor <hancarldemotor@gmail.com>
2025-08-27 09:47:24 +0800

    first commit

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGBiE8KCY0fgCVHJyTq58fVhyB2SQSJ4E",
+  authDomain: "jcdgoals.firebaseapp.com",
+  projectId: "jcdgoals",
+  storageBucket: "jcdgoals.firebasestorage.app",
+  messagingSenderId: "628335833927",
+  appId: "1:628335833927:web:f1474ab5e07f4c7e2d5262"
+};
+
+
+const app = initializeApp(firebaseConfig);
